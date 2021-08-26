36f641e47277ef14500e6314da63c68500919bf7
gilbertlc <gilbert.lacasandile@gmail.com>
2021-08-26 16:01:05 +0800

    put SECRET_KEY to env

--- gilbertlc_api/settings.py
+++ gilbertlc_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-so=agm^s5a18wrc3ivzlqfy0qe723g$wc1jh8s5ne%^gms8%xc'
+# SECRET_KEY = 'django-insecure-so=agm^s5a18wrc3ivzlqfy0qe723g$wc1jh8s5ne%^gms8%xc'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
