c60e59cbc7b1be2892849019df418a996746a8d0
gilbertlc <gilbert.lacasandile@gmail.com>
2021-08-29 21:43:20 +0800

    setup file upload to s3 w/ django-storages & boto3

--- gilbertlc_api/settings.py
+++ gilbertlc_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'portfolio',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,14 @@ MEDIA_URL = '/files/'
 CORS_ALLOW_ALL_ORIGINS = True
 
 django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+
+AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_ADDRESSING_STYLE = 'virtual'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
