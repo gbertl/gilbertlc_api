effd6214abe74c81385231135cf54619fb456257
gilbertlc <gilbert.lacasandile@gmail.com>
2021-10-08 12:59:47 +0800

    use api_view & Response for views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,18 +1,21 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from portfolio.serializers import CategorySerializer, ProjectSerializer
 from .models import Project, Category
 
 
+@api_view(['GET'])
 def index(request):
     projects = Project.objects.order_by('priority_order')
     serializer = ProjectSerializer(projects, many=True)
 
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
 
 
+@api_view(['GET'])
 def categories(request):
     categories = Category.objects.order_by('id')
     serializer = CategorySerializer(categories, many=True)
 
-    return JsonResponse(serializer.data, safe=False)
+    return Response(serializer.data)
