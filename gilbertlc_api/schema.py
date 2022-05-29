78880be5de9d75a46e3fa3450bd8c025363d6669
gbertl <gilbertlcsndle@gmail.com>
2022-05-29 15:02:03 +0800

    implement orderby

--- gilbertlc_api/schema.py
+++ gilbertlc_api/schema.py
@@ -2,13 +2,16 @@ from graphene_django import DjangoObjectType
 import graphene
 from portfolio.models import Category, Project, Screenshot, Technology
 
+
 class TechnologyNode(DjangoObjectType):
     class Meta:
         model = Technology
 
+
 class CategoryNode(DjangoObjectType):
     class Meta:
         model = Category
+
 
 class ScreenshotNode(DjangoObjectType):
     class Meta:
@@ -17,7 +20,9 @@ class ScreenshotNode(DjangoObjectType):
 class ProjectNode(DjangoObjectType):
     technology_list = graphene.List(graphene.String)
     category_list = graphene.List(graphene.String)
-    screenshot_list = graphene.List(graphene.String)
+    screenshot_list = graphene.List(
+        graphene.String, order_by=graphene.List(graphene.String)
+    )
 
     class Meta:
         model = Project
@@ -28,19 +33,35 @@ class ProjectNode(DjangoObjectType):
     def resolve_category_list(self, _):
         return [category.name for category in self.categories.all()]
 
-    def resolve_screenshot_list(self, _):
-        return [sc.image.url for sc in Screenshot.objects.filter(project_id=self.id)]
+    def resolve_screenshot_list(self, _, **args):
+        order_by = args.get("order_by")
+
+        if order_by:
+            return [
+                screenshot.image.url
+                for screenshot in self.screenshots.order_by(*order_by)
+            ]
+
+        return [screenshot.image.url for screenshot in self.screenshots.all()]
 
 
 class Query(graphene.ObjectType):
-    projects = graphene.List(ProjectNode)
-    categories = graphene.List(CategoryNode)
+    projects = graphene.List(ProjectNode, order_by=graphene.List(graphene.String))
+    categories = graphene.List(CategoryNode, order_by=graphene.List(graphene.String))
 
+    def resolve_projects(self, _, **args):
+        order_by = args.get("order_by")
 
-    def resolve_projects(self, _):
+        if order_by:
+            return Project.objects.order_by(*order_by)
         return Project.objects.all()
 
-    def resolve_categories(self, _):
+    def resolve_categories(self, _, **args):
+        order_by = args.get("order_by")
+
+        if order_by:
+            return Category.objects.order_by(*order_by)
         return Category.objects.all()
+
 
 schema = graphene.Schema(query=Query)
